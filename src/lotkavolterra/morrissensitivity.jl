d8b1545345c8425c3fd7cbfb0dc11eb4d7ef084d
piluc <pierluigi.crescenzi@gssi.it>
2023-12-11 18:14:14 +0100

    Start Sobol method

--- src/lotkavolterra/morrissensitivity.jl
+++ src/lotkavolterra/morrissensitivity.jl
@@ -17,6 +17,7 @@ function modify(p_from_gsa)
     @named lk = LotkaVolterra.lotka_volterra()
     t = collect(range(0, stop=10, length=200))
     prob1 = remake(prob; p=p_from_gsa)
+    println(p_from_gsa)
     sol = solve(prob1, Tsit5(); saveat=t)
     return [sol[lk.U3][1], sol[lk.U3][100], sol[lk.U3][200]]
 end
@@ -25,8 +26,8 @@ function upper_lower_bounds(prob)
     y = []
     push!(y, [prob.p[1] - 0.5, prob.p[1] + 0.5])
     for i in 2:lastindex(prob.p)
-        # push!(y, [prob.p[i] - 0.5, prob.p[i] + 0.5])
-        push!(y, [prob.p[i], prob.p[i]])
+        push!(y, [prob.p[i] - 0.5, prob.p[i] + 0.5])
+        # push!(y, [prob.p[i], prob.p[i]])
     end
     return y
 end
@@ -34,7 +35,7 @@ end
 function execute_gsa(tnt, nt, pl)
     prob = lk_ode_problem()
     ub_lb = upper_lower_bounds(prob)
-    return gsa(modify, Morris(total_num_trajectory=tnt, num_trajectory=nt, len_design_mat=pl), ub_lb)
+    return gsa(modify, Morris(relative_scale=true, total_num_trajectory=tnt, num_trajectory=nt, len_design_mat=pl), ub_lb)
 end
 
 # execute_gsa(1000, 150);
